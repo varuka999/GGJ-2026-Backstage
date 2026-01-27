e509d8a863b492fc2d44a00b90aac58fbba521c6
ArthurH-cmd <arthurheymans101@gmail.com>
2026-01-26 17:22:37 -0800

    basic animations added to player preFab

--- GameJam_2026/Assets/Scripts/PlayerController.cs
+++ GameJam_2026/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour
     private InputAction interactAction = null;
     [SerializeField] float moveSpeed = 8.0f;
     private Rigidbody2D rb = null;
+    private Animator animator = null;
+    private Vector2 animatorDirection = Vector2.down;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -18,6 +20,10 @@ public class PlayerController : MonoBehaviour
         moveAction = input.Player.Move;
         interactAction = input.Player.Interact;
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        animator.SetFloat("X", 0);
+        animator.SetFloat("Y", 0);
+        animator.SetBool("isMoving", false);
     }
 
     void OnEnable()
@@ -39,9 +45,44 @@ public class PlayerController : MonoBehaviour
     {
         //Movement code
         Vector2 moveInput = moveAction.ReadValue<Vector2>();
+
+        ChangeAnimationDirection(moveInput);
+        
+
         Vector3 direction = moveInput.normalized;
         Debug.Log(moveInput);
         rb.linearVelocity = direction * moveSpeed;
 
+    }
+
+
+
+    void ChangeAnimationDirection(Vector2 moveInput) 
+    {
+        if (moveInput.y == 1 || moveInput.y == -1) 
+        {
+            Vector3 direction = Vector3.zero;
+            direction.y += moveInput.y;
+            animatorDirection = direction;
+        }
+
+        else if (moveInput.x == 1 || moveInput.x == -1) 
+        {
+            Vector3 direction = Vector3.zero;
+            direction.x += moveInput.x;
+            animatorDirection = direction;
+        }
+
+        if (moveInput == Vector2.zero) 
+        {
+            animator.SetBool("isMoving", false);
+        } 
+        else 
+        {
+            animator.SetBool("isMoving", true);
+        }
+
+        animator.SetFloat("X", animatorDirection.x);
+        animator.SetFloat("Y", animatorDirection.y);
     }
 }
